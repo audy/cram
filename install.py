0f368f9fde7da728db79803e4acbf9c592b9be0c
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-07-18 15:36:46 -0400

    beginnings

---
+++ install.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# get all necessary source codes
+
+# get all necessary data bases
--- run.py
+++ run.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
 
+# assemble with stitch
+
+# assemble with velvet
+
+
+# predict ORFs
+
+# identify ORFs
+
+    # obtain coverage of ORFs
+
+# separate unidentified ORFs
+
+    # reference assemble to TaxCollector database
