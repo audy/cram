20afd07282937003fb22db565fa67d1ed6465b52
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-07-21 15:48:12 -0400

    misc. tools

---
+++ misc/flatten_phmmer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# PHMMER does not let you report only the best hit.
+# I have 11GB Phmmer files :(
+
+# Austin
+
+
+import sys
+
+lastquery = False
+
+with open(sys.argv[1]) as handle:
+    for line in handle:
+        if line.startswith('#'):
+            print(line.strip())
+            continue
+        line = line.split()
+        query = line[2]
+        if lastquery and query != lastquery:
+            print(' '.join(line))
+        lastquery = query
+
