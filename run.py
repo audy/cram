26f65ed8132c7c3dd1529711d0b80b4c9eaa18fe
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-07-27 11:20:57 -0400

    many changes. Importantly, I was using the wrong output file from Velvet and now that is fixed

--- run.py
+++ run.py
@@ -14,76 +14,80 @@ d = get_outdir(Config.out)
 
 ohai('running pipeline!')
 
-## MAKE DIRECTORIES
-[ run('mkdir -p %s' % i) for i in [
-    out,
-    d('orfs'),
-    d('anno'),
-    d('refs')] ]
-
+# ## MAKE DIRECTORIES
+# [ run('mkdir -p %s' % i) for i in [
+#     out,
+#     d('orfs'),
+#     d('anno'),
+#     d('refs')] ]
+# 
 # ## TRIM READS
 # ohai('trimming sequences')
 # sequences = (r for r in Dna(open(reads), type='fastq'))
 # trimmed = (Trim.trim(r) for r in sequences)
+# # filter by minimum length (no need for this w/ Velvet?)
 # trimmed = (i for i in trimmed if len(i) > cutoff)
-# 
+#  
 # with open(d('reads_trimmed.txt'), 'w') as handle:
 #     for t in trimmed:
 #         print >> handle, t.fasta
-#         
+# 
 # run('grep -c \'^>\' %s' % d('reads_trimmed.txt'))
-
-## ASSEMBLE W/ VELVET
-kmers = {
-    35: d('contigs_35'),
-    45: d('contigs_45'),
-    55: d('contigs_55')
-}
-
-for kmer in kmers:
-    velvet(
-        reads  = d('reads_trimmed.txt'),
-        outdir = kmers[kmer],
-        kmer   = kmer
-    )
-
+# 
+# ASSEMBLE W/ VELVET
+# kmers = {
+#      31: d('contigs_31'),
+#      51: d('contigs_51'),
+#      71: d('contigs_71')
+# }
+# 
+# for kmer in kmers:
+#     velvet(
+#         reads  = d('reads_trimmed.txt'),
+#         outdir = kmers[kmer],
+#         kmer   = kmer
+#     )
+#     # calculate n50 and produce a histogram
+#     run('misc/n50.py %s contig_lenths_%s.pdf' % (kmers[kmer]/contigs.fa, kmer))
+# 
 # concatenate all contigs
-run('cat %s > %s' % (
-    ' '.join(i + '/Sequences' for i in kmers.values()),
-    d('joined_contigs.txt'))
-)
+# TODO: remove duplicate contigs!!?
+# run('cat %s > %s' % (
+#     ' '.join(i + '/contigs.fa' for i in kmers.values()),
+#     d('joined_contigs.txt'))
+# )
 
 # run final assembly
-velvet(
-    reads    = d('joined_contigs.txt'),
-    outdir   = d('final_contigs'),
-    kmer     = 35
-)
+# velvet(
+#     reads    = d('reads_trimmed.txt'),
+#     outdir   = d('final_contigs'),
+#     kmer     = 51
+# )
 
-## PREDICT OPEN READING FRAMES
-prodigal(
-    input  = d('final_contigs/Sequences'),
-    out    = d('orfs/predicted_orfs') # prefix*
-)
+# ## PREDICT OPEN READING FRAMES
+# prodigal(
+#     input  = d('final_contigs/contigs.fa'),
+#     out    = d('orfs/predicted_orfs') # prefix*
+# )
 
 # create table connecting seed and subsystems
 # seed_sequence_number -> system;subsystem;subsubsystem;enzyme
 # use this later to make functions tables
 
-prepare_seed(
-    seed = 'db/seed.fasta',
-    peg  = 'db/subsystems2peg',
-    role = 'db/subsystems2role',
-    out  = 'db/seed_ss.txt'
-)
-
+# prepare_seed(
+#     seed = 'db/seed.fasta',
+#     peg  = 'db/subsystems2peg',
+#     role = 'db/subsystems2role',
+#     out  = 'db/seed_ss.txt'
+# )
+# 
 ## IDENTIFY ORFS WITH PHMMER
-phmmer(
+phmmer( 
     query = d('orfs/predicted_orfs.faa'),
     db    = 'db/seed.fasta',
     out   = d('anno/proteins.txt')
 )
-
+quit()
 ## GET ORF COVERAGE
 orf_coverage = reference_assemble(
     query = 'reads.txt',
