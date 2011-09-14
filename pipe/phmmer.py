bb503a4f36dd20c962c3977144e7cb955b950708
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-09-14 15:03:16 -0400

    phmmer sort of test

--- pipe/phmmer.py
+++ pipe/phmmer.py
@@ -1,9 +1,14 @@
-# for using PHMMER
+# encoding: utf-8
 
 from helps import *
 
 def phmmer(**ops):
-    ''' run phmmer '''
+    ''' Runs phmmer.
+    # This fails because of relative directory bull-ish. Gotta fix this.
+    # >>> phmmer(query = '../test/test.fasta', db = '../test/sample_seed.fna', out = '../test/test_phmmer.out' )
+    # ✪ running phmmer: ../test/test.fasta vs. ../test/sample_seed.fna
+    # ✓ complete
+    '''
     
     # phmmer doesn't like to use as many cpus as you specify
     # so it would be a good idea to put some kind of simple
@@ -15,7 +20,7 @@ def phmmer(**ops):
     # this up.
     
     phmmer = ' '.join([
-        'bin/phmmer',
+        '/bin/phmmer',
         '--notextw',
         '--domE 0.001',
         '--incE 0.00001',
@@ -32,3 +37,7 @@ def phmmer(**ops):
     
     ohai('running phmmer: %(query)s vs. %(db)s' % ops)
     run(phmmer, generates=ops['out'] + '.table')
+    
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
