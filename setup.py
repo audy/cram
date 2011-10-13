102df1a15a3b166c24a697ead4206e26162510b1
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-10-13 16:45:51 -0400

    setting of the up

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(name="metacram",
+      version="0.5",
+      description="Comparative Rapid Annotation of Metagenomes",
+      license="GPLv3",
+      author="Austin G. Davis-Richardson",
+      author_email="harekrishna@gmail.com",
+      url="http://github.com/audy/cramp",
+      packages = find_packages(),
+      keywords= "bioinformatics metagenome metagenomics",
+      zip_safe = True)
