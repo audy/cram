8f9646a6f361ad17058f543ed618b63c553a5f90
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-10-24 13:36:58 -0400

    merged branch reorgo (reorganization) with master

---
+++ metacram/pipe.py
@@ -0,0 +1,153 @@
+# encoding: utf-8
+
+import sys
+
+from dnaio import *
+from helps import *
+
+def prepare_seed(**ops):
+    ''' Create table of seed_id -> subsystems for use later when creating
+    subsystem coverage tables with make_subsystem_table()
+    
+    >>> prepare_seed(
+    ...    seed = '../test/sample_seed.fna',
+    ...    peg  = '../test/subsystems2peg',
+    ...    role = '../test/subsystems2role',
+    ...    out  = '/dev/null'
+    ... )
+     ✪ preparing SEED subsystems database
+    '''
+    
+    ohai('preparing SEED subsystems database')
+    if os.path.exists(ops['out']) and '/dev' not in ops['out']:
+        okay('skipping')
+        return
+        
+    # TODO work out better fig id parsing?
+    
+    # load subsystems from figids using subsystems2peg
+    figs_to_name = {}
+    with open(ops['peg']) as handle:
+        for line in handle:
+            line = line.strip()
+            line = line.split('\t')
+            
+            assert(len(line) == 3)
+            
+            _, name, fig = line
+            
+            assert type(fig) == str
+            
+            figs_to_name[fig] = name
+    
+    # load full subsystem names using subsystems2role
+    name_to_ss = {}
+    with open(ops['role']) as handle:
+        for line in handle:
+            line = line.strip()
+            line = line.split('\t')
+            
+            assert(len(line) == 4)
+            
+            a, c, b, d = line[1], line[2], line[0], line[3]
+            
+            name_to_ss[d] = [a, b, c]
+    
+    # Print table, using SEED headers
+    with open(ops['seed']) as handle:
+        with open(ops['out'], 'w') as out:
+            for line in handle:
+                if line.startswith('>'):
+                
+                    fig = line.split()[0][1:]
+                    name = figs_to_name.get(fig, fig)
+                    ss = name_to_ss.get(name, 'NA')
+                
+                    if ss == 'NA':
+                        names = 'NA;%s' % name
+                    else:
+                        names = '%s;%s;%s;%s' % (ss[0], ss[1], ss[2], name)
+                
+                    print >> out, "%s\t%s" % (fig, names)
+
+def subsystems_table(**ops):
+    ''' converts a coverage table to a subsystems table given
+    the figid -> subsystems info
+    
+    '''
+
+    ohai('generating subsystems coverage table')
+    
+    # TODO: I used to take into account CLC's paired-end data and get the
+    # LCA of the two proteins if they didn't match. However, I stopped doing
+    # this for two reasons:
+    #
+    # 1.) It doesn't make sense: If mates in a pair match different references
+    # they are unlikely to match different references with similar functions
+    # (I saw this when looking at the output)
+    #
+    # 2.) CLCs output for paired-data has bugs and is meaningless
+        
+    subsnames      = ops['subsnames']
+    coverage_table = ops['coverage_table']
+    out            = ops['out']
+    
+    ohai('creating subsystems table')
+    if os.path.exists(out):
+        okay('skipping')
+        return
+    
+    # load subsystem names
+    with open(subsnames) as handle:
+        fig_to_name = {}
+        for line in handle:
+            if line.startswith('#'): continue
+            
+            figid, name = line.strip().split('\t')
+            
+            # what if this happens?
+            if figid in fig_to_name:
+                    assert name == fig_to_name[figid]
+            
+            fig_to_name[figid] = name
+    
+    # the output table should look like this:
+    # sys_a,\t10
+    # sys_a;sys_a.1\t5
+    # sys_a;sys_a.2\t5 etc...
+    
+    # parse coverage table and output hierarchies based on SEED subsystems
+    from collections import defaultdict
+    merged_counts = defaultdict(int)
+    
+    with open(coverage_table) as handle:
+        for line in handle:
+            if line.startswith('#'): continue
+            figid, count = line.strip().split('\t')
+            count = int(count)
+            
+            # use figid to get the ss name, or just keep the figid
+            # that usually means that the ORF was not identified
+            # in the SEED database and it's still useful to have
+            # them in the subsystems table. However, we append
+            # 'unidentified' at the beginning
+            subsystems = fig_to_name.get(figid, figid).split(';')
+            
+            # This merges subsystem hierarchies and sums their counts
+            # TODO I really ought to create a test for this as it's pretty
+            # crucial and breakeable
+            for i in range(len(subsystems)):
+              for i, s in enumerate(subsystems):
+                  if s == '':
+                      subsystems[i] = '-'
+                  hierarchy = ';'.join(subsystems[:i])
+                  merged_counts[hierarchy] += count
+    
+    with open(out, 'w') as handle:
+        for s in sorted(merged_counts, key = lambda x: merged_counts[x], reverse=True):
+            print >> handle, "%s\t%s" % (s, merged_counts[s])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ metacram/pipelines/illumina.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import os
+from glob import glob
+
+from metacram import *
+
+def run(**args):
+    ''' the actual pipeline '''
+    left_mates = args['left_mates'] # glob('data/left*')
+    right_mates = args['right_mates'] # glob('data/right*')
+    
+    out = args.get('out', 'out')
+    read_format = args.get('read_format')
+    
+    # location of databases
+    db = args.get('db',{
+        'seed': '~/cram/db/seed.fasta',
+        'taxcollector': '~/cram/db/taxcollector.fa',
+        'subsystems2peg': '~/cram/db/subsystems2peg',
+        'subsystems2role': '~/cram/db/subsystems2role',
+        'seed_ss': '~/cram/db/seed_ss.txt',
+    })
+    
+    # name of reference assembler
+    # (CLC used to be supported)
+    ref = 'SMALT'
+    
+    # expand tilde to home directory
+    for k in db:
+        db[k] = os.path.expanduser(db[k])
+
+    # Creates a simple function to prepend the output directory
+    # to the directory/filename you specify
+    d = get_outdir(out)
+
+    # Define how to filter taxonomic matches
+    phylo = {
+        'phylum': { 'num': 2, 'sim': 0.80 },
+        'genus': { 'num': 8, 'sim': 0.95 },
+    }
+
+    ohai('running pipeline!')
+
+    ## MAKE DIRECTORIES
+    [ run('mkdir -p %s' % i) for i in [
+        out,
+        d('orfs'),
+        d('anno'),
+        d('refs'),
+        d('tables'),
+        d('trimmed') ] ]
+
+    ## TRIM PAIRS BASED ON QUALITY SCORES
+    counts = trim_pairs(
+        left_mates   = [ open(i) for i in left_mates ],
+        right_mates  = [ open(i) for i in right_mates ],
+        input_format = READ_FORMAT,
+        out_left     = d('trimmed/singletons_left.fastq'),
+        out_right    = d('trimmed/singletons_right.fastq'),
+        out          = d('trimmed/reads_trimmed.fastq'),
+        cutoff       = 70
+    )
+
+    ## ASSEMBLE WITH VELVET
+    # 3 sub assemblies:
+    kmers = {
+         31: d('contigs_31'),
+         51: d('contigs_51'),
+         71: d('contigs_71')
+    }
+
+    [ velvet(
+        reads = [
+            ('fastq', 'shortPaired', d('trimmed/reads_trimmed.fastq')),
+            ('fastq', 'short', d('trimmed/singletons_left.fastq')),
+            ('fastq', 'short', d('trimmed/singletons_right.fastq'))
+        ],
+        outdir = kmers[k],
+        k      = k
+    ) for k in kmers ]
+
+    # run final assembly
+    velvet(
+        reads    = [('fasta', 'long', d('contigs_%s/contigs.fa' % k)) for k in kmers],
+        outdir   = d('contigs_final'),
+        k        = 51
+    )
+
+    ## PREDICT OPEN READING FRAMES
+    prodigal(
+        input  = d('contigs_final/contigs.fa'),
+        out    = d('orfs/predicted_orfs') # prefix*
+    )
+
+    ## IDENTIFY ORFS WITH PHMMER
+    phmmer( 
+        query = d('orfs/predicted_orfs.faa'),
+        db    = db['seed'],
+        out   = d('anno/proteins.txt')
+    )
+
+    # flatten phmmer file (we only need top hit)
+    run('misc/flatten_phmmer.py out/anno/proteins.txt.table > out/anno/proteins_flat.txt',
+        generates='out/anno/proteins_flat.txt')
+
+    ## GET ORF COVERAGE using CLC
+
+    # create table connecting seed and subsystems
+    # seed_sequence_number -> system;subsystem;subsubsystem;enzyme
+    # use this later to make functions tables
+    # index reference database
+    smalt_index(
+        reference = d('orfs/predicted_orfs.fna'),
+        name      = d('orfs/predicted_orfs')
+    )
+
+    # reference assemble
+    queries = glob('out/*.fastq') 
+    for q in queries:
+        ohai('smalt mapping %s' % q)
+        smalt_map(
+            query     = q,
+            reference = d('orfs/predicted_orfs'),
+            out       = d('refs/%s_vs_orfs.cigar' % os.path.basename(q)),
+            identity  = 0.80
+        )
+    
+        ohai('coverage table %s' % q)
+        # make coverage table
+        smalt_coverage_table(
+            assembly = d('refs/%s_vs_orfs.cigar' % os.path.basename(q)),
+            phmmer   = d('anno/proteins_flat.txt'),
+            out      = d('tables/%s_seed_coverage.txt' % os.path.basename(q))
+        )
+
+    # concatenate assembly coverage tables
+    ohai('concatenating assembly coverage tables')
+
+    # TODO make this nicer:
+    coverage_tables = glob(d('tables/*_seed_coverage.txt'))
+
+    run('cat %s > %s' % \
+        (' '.join(coverage_tables), d('tables/SMALT_orfs_coverage.txt')),
+        generates=d('tables/SMALT_orfs_coverage.txt')
+    )
+
+    prepare_seed(
+        seed = db['seed'],
+        peg  = db['subsystems2peg'],
+        role = db['subsystems2role'],
+        out  = db['seed_ss']
+    )
+
+    subsystems_table(
+        subsnames      = db['seed_ss'],
+        coverage_table = d('tables/%s_orfs_coverage.txt' % ref),
+        out            = d('tables/%s_subsystems_coverage.txt' % ref),
+        reads_type     = 'fastq',
+    )
+
+    ## GET OTU COVERAGE
+    ohai('building index of taxcollector database')
+    smalt_index(
+        reference = db['taxcollector'], 
+        name      = '~/cram/db/taxcollector'
+    )
+
+    # reference assemble
+    queries = glob('out/*.fastq') 
+    for q in queries:
+        ohai('smalt mapping %s' % q)
+        smalt_map(
+            query     = q,
+            reference = '~/cram/db/taxcollector',
+            out       = d('refs/%s_vs_taxcollector.cigar' % os.path.basename(q)),
+            identity  = 0.80
+        )
+    
+        ohai('coverage table %s' % q)
+        # make coverage table
+        smalt_coverage_table(
+            assembly = d('refs/%s_vs_taxcollector.cigar' % os.path.basename(q)),
+            out      = d('tables/%s_otu_coverage.txt' % os.path.basename(q))
+        )
+
+    coverage_tables = glob(d('tables/*_otu_coverage.txt'))
+    ohai('concatenating OTU coverage tables')
+    run('cat %s > %s' % \
+        (' '.join(coverage_tables), d('tables/SMALT_otu_coverage.txt')),
+        generates=d('tables/SMALT_otu_coverage.txt')
+    )
+
+if __name__ == '__main__':
+    run(
+        left_mates  = glob('data/left*'),
+        right_mates = glob('data/right*'),
+        read_format = 'qseq'
+    )
