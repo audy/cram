e31a10ecaf30043d3ec39e1ae4a2dc9baea15e74
audy <harekrishna@gmail.com>
2013-09-13 16:27:01 -0400

    removed trailing whitespace

--- metacram/runner.py
+++ metacram/runner.py
@@ -5,33 +5,33 @@ from logger import *
 def run(cmd, generates=False, force=False, silent=False, sgi=False):
     ''' Runs a system command unless output exists unless forced.
     Prints a message when it's done'
-    
+
     # run a command
     >>> run('ls > /dev/null')
     ls > /dev/null
-    
+
     # run a command unless a file already exists
     >>> run('ls helps.py', generates='helps.py')
     skipping
-    
+
     # run a command and force it
     # (same as taking out the generates)
     >>> run('ls helps.py > /dev/null', generates='helps.py', force=True)
     forced
     complete
-    
+
     # This should raise an exception
     # >>> run('thisisnotacommand')
     # Traceback (most recent call last):
     #     ...
     # Exception, thisisnotacommand
-    
-         
+
+
     # remove the log file without logging or printing a message or logging :)
     >>> run('rm log.txt', silent=True)
-    
+
     '''
-    
+
     # check if output already exists, skip if it does.
     if generates and not force:
         if type(generates) == str:
@@ -40,16 +40,16 @@ def run(cmd, generates=False, force=False, silent=False, sgi=False):
             # dont skip if output is to std*
             if f in ['/dev/null', '/dev/stdout', '/dev/stderr']:
                 break
-            if os.path.exists(f) and not silent:    
+            if os.path.exists(f) and not silent:
                 okay('skipping')
                 return
     if force:
         okay('forced')
-    
+
     # RUN COMMAND
     try:
         res = subprocess.call(cmd.split())
-    
+
     # COMMAND FAILED
     except CalledProcessError:
         if generates:
@@ -58,7 +58,7 @@ def run(cmd, generates=False, force=False, silent=False, sgi=False):
                     os.unlink(f)
         if not silent:
             ohno(cmd)
-    
+
     # IT'S ALL GOOD
     else:
         if generates:
